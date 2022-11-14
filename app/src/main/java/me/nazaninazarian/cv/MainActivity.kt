b5b723c05baed1a4d44a26b3acb98528c24742bf
Nazanin Azarian <badi.mojgan@gmail.com>
2022-11-14 22:13:37 +0330

    Add call me button

--- app/src/main/java/me/nazaninazarian/cv/MainActivity.kt
+++ app/src/main/java/me/nazaninazarian/cv/MainActivity.kt
@@ -1,11 +1,21 @@
 package me.nazaninazarian.cv
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun call(view: View){
+        val callIntent = Intent(Intent.ACTION_CALL)
+        callIntent.data = Uri.parse("tel:09198923977")
+        startActivity(callIntent)
     }
 }
